fbe17653da4c6b29d43325d455fa5ba51de18d79
Doris Morales <moralesdoris880@gmail.com>
2022-03-18 14:48:46 -0400

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence:true
+    validates :content, length: { minimum:250 }
+    validates :summary,length: { maximum:250 }
+    validates :category, inclusion: {in: ["Fiction","Non-Fiction"]}
+    validate :no_clickbait
+
+    def no_clickbait
+        if title === "True Facts"
+            errors.add(:title, "No Clickbait Titles")
+        end
+    end
 end
